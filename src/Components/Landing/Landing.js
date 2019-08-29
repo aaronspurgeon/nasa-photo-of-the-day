d9a6e610e9cbab06b97132fdd144e71e6d611d67
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-29 19:53:13 -0400

    tried implementing a read more library, i dont think it works with the data from api

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import ReadMoreReact from "read-more-react";
 
 
 const Wrapper = styled.section`
@@ -8,7 +9,7 @@ const Wrapper = styled.section`
 `
 
 const LandingH1 = styled.h1`
-    font-size: 3rem;
+    font-size: 4rem;
     font-weight: 200;
 `
 const Date = styled.span`
@@ -18,23 +19,33 @@ const Date = styled.span`
     float: right;
 `
 
+const ImageWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 const Landing = (props) => {
     const urlStyle = {
-        width: 'auto',
+        width: '900px',
         height: '600px'
     }
-    console.log(props);
     
+    const text = props.data.explanation;
+    console.log(text);
     
     return (
         <Wrapper>
             <LandingH1>{props.data.title}<Date>{props.data.date}</Date></LandingH1>
-            {
-                props.data.media_type === "video"
-                    ? <iframe title="nasaVideo" style={urlStyle} src={props.data.url}></iframe>
-                    : <img style={urlStyle} src={props.data.url} alt="awesome pic of space"/>
-            }
-            <p>{props.data.explanation}</p>
+            <ImageWrapper>
+                {
+                    props.data.media_type === "video"
+                        ? <iframe title="nasaVideo" style={urlStyle} src={props.data.url}></iframe>
+                        : <img style={urlStyle} src={props.data.url} alt="awesome pic of space"/>
+                }
+            </ImageWrapper>
+            
+            <p>{text}</p>
+            
         </Wrapper>
     );
 };
